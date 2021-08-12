7b13cc976adc4c9c69d0c48226be3d13de35c317
firozasheik22 <firozasheik22@gmail.com>
2021-08-12 09:48:33 +0530

    first commit

---
+++ hw/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for hw project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw.settings')
+
+application = get_wsgi_application()
+
+
